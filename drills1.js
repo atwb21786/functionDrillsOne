a29c62e2e5a590cdeb62ee88f3d2dbfd89c8df69
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-10 19:20:11 -0700

    YOB function

--- drills1.js
+++ drills1.js
@@ -1,5 +1,11 @@
 'use strict';
 
+function getYearOfBirth(age) {
+  return 2020 - age;
+}
+
+console.log(getYearOfBirth(33));
+
 function createGreeting(name, age) {
   const yearOfBirth = 2020 - age;
   return `Hi my name is ${name} and I\'m ${age} years old.  I was born in ${yearOfBirth}.`;
