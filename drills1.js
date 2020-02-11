2f5a05a120e31487fd848f0f6ce1d754e42af8ec
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-02-10 18:58:55 -0700

    return replaces console

--- drills1.js
+++ drills1.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting() {
-    console.log('Hi my name is Andrew and I\'m 33 years old');
+    return 'Hi my name is Andrew and I\'m 33 years old';
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
